b24827e64c7062ec5f6e8c76802d217c66d169cc
Andrew Chilton <andychilton@gmail.com>
2012-04-19 16:41:14 +1200

    Initial layout of gane

---
+++ games/slider/ready.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    // for all of the tiles, add the background image
+    var img = 'img/elephant.png';
+    $('.tile').css({
+        'background-image' : 'url(' + img + ')'
+    });
+
+});
